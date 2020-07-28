c3b7b7540f5646eefab4c2237fa9dd739cf52365
Dreeam-qwq <61569423+Dreeam-qwq@users.noreply.github.com>
2020-07-28 21:02:11 +0800

    LiquidBounce China!

---
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/ModuleCategory.kt
@@ -0,0 +1,19 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module
+
+enum class ModuleCategory(val displayName: String) {
+
+    COMBAT("战斗类"),
+    PLAYER("玩家类"),
+    MOVEMENT("移动类"),
+    RENDER("显示类"),
+    WORLD("世界类"),
+    MISC("杂类"),
+    EXPLOIT("漏洞类"),
+    FUN("趣味类")
+
+}
